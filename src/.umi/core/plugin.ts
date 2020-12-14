ae10c122feae8787829e9784ae2aeb71d01d8c68
daxiong <417170808@qq.com>
2020-12-15 00:00:23 +0800

    first commit v5

---
+++ src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/Users/daxiong/Desktop/localhost/web-vue/vue-admin-antd/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','getInitialState','initialStateConfig','locale','locale','layout','request',],
+});
+
+export { plugin };
---
+++ src/.umi/plugin-helmet/exports.ts
@@ -0,0 +1,3 @@
+// @ts-nocheck
+// @ts-ignore
+export { Helmet } from '/Users/daxiong/Desktop/localhost/web-vue/vue-admin-antd/node_modules/react-helmet';
