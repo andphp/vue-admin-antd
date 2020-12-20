384b61a401087a60d50fca2a4abde206ad6d3cd5
daxiong <417170808@qq.com>
2020-12-21 00:46:23 +0800

    update antd-pro-vue v2

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from 'D:/vue/test-antd/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','getInitialState','initialStateConfig','locale','locale','layout','request',],
-});
-
-export { plugin };
--- src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from 'D:/vue/test-antd/node_modules/react-helmet';
